591ea3ca65dd9547d0432e1447997562fcf50d55
krishnendu basak <krisauz17@yahoo.com>
2022-10-02 14:28:16 +1100

    phase-1-javascript-variables-lab is done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+//const fs = ;
+/*function companyName () {
+    const companyName = "Scuber";
+}
+function mostProfitableNeighborhood() {
+    let mostProfitableNeighborhood = "Chelsea";
+}
+function companyCeo () {
+    let companyCeo = "Susan Smith";
+}
+companyName();
+mostProfitableNeighborhood();
+companyCeo();*/
+
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
